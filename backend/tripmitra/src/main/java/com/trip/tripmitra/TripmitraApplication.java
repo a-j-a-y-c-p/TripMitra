8d422714cdcaf0e8d2a3ef91ae1ff02cda493c1b
Ajay Saini <ajaysaini0505@gmail.com>
2025-06-24 00:50:40 +0530

    initial setup

---
+++ backend/tripmitra/src/main/java/com/trip/tripmitra/TripmitraApplication.java
@@ -0,0 +1,13 @@
+package com.trip.tripmitra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripmitraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripmitraApplication.class, args);
+	}
+
+}
---
+++ backend/tripmitra/src/test/java/com/trip/tripmitra/TripmitraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trip.tripmitra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripmitraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
