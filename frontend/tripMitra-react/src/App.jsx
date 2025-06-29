ee2cbc4a9c756cfe38152ce3f3a589f351a47c51
Ajay Saini <ajaysaini0505@gmail.com>
2025-06-29 23:10:16 +0530

    changed project structure

--- frontend/tripMitra-react/src/App.jsx
+++ frontend/tripMitra-react/src/App.jsx
@@ -1,36 +1,14 @@
-import { useState } from 'react'
 import './App.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Home from './components/Home.jsx'
-import Login from './components/Login.jsx'
-import Dashboard from './components/Dashboard.jsx'
+import { Outlet } from 'react-router-dom'
 import Navbar from './components/Navbar.jsx'
 
 
-
-
 function App() {
-  const [count, setCount] = useState(0)
-
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element:<><Navbar/><Home/></> 
-    },
-    {
-      path: "/login",
-      element: <><Navbar/><Login/></>
-    },
-    {
-      path: "/dashboard",
-      element: <><Navbar/><Dashboard/></>
-    }
-  ])
-
   return (
-    <>
-      <RouterProvider router={router} />
-    </>
+    <div>
+      <Navbar/>
+      <Outlet />
+    </div>
   )
 }
 
--- frontend/tripMitra-react/src/main.jsx
+++ frontend/tripMitra-react/src/main.jsx
@@ -2,13 +2,26 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Home from './routes/Home.jsx'
+import Dashboard from './routes/Dashboard.jsx'
+import Login from './routes/Login.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: 'dashboard', element: <Dashboard /> },
+      { path: 'login', element: <Login /> },
+    ],
+  }
+])
 
 
 createRoot(document.getElementById('root')).render(
-
-
-
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
