dcfe1c6a4b59b6b6908b5e1d9132c728acc3581e
Harshit Srivastava <72arshit@gmail.com>
2025-07-21 20:18:37 +0530

    user services updated

--- backend/tripmitra/src/main/java/com/acts/tripmitra/dto/UserDto.java
+++ backend/tripmitra/src/main/java/com/acts/tripmitra/dto/UserDto.java
@@ -1,7 +1,24 @@
 package com.acts.tripmitra.dto;
 
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class UserDto {
 
-	
+
+    private String userEmail;
+
+    private String userName;
+
+    private String userPassword;
+
+    private String userRole;
 	
 }
--- backend/tripmitra/src/main/java/com/acts/tripmitra/entity/User.java
+++ backend/tripmitra/src/main/java/com/acts/tripmitra/entity/User.java
@@ -1,17 +1,37 @@
 package com.acts.tripmitra.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+
 @Entity
-@Table(name="users")
+@Table(name = "users")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
 
-	
+    @Id
+    @GeneratedValue(generator="increment")
+    @Column(name = "userid")
+    private Integer userId;
+
+    @Column(name = "useremail", nullable = false, unique = true)
+    private String userEmail;
+
+    @Column(name = "username", nullable = false)
+    private String userName;
+
+    @Column(name = "userpassword", nullable = false)
+    private String userPassword;
+
+    @Column(name = "userrole", nullable = false)
+    private String userRole;
 }
+
