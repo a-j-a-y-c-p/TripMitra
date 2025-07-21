a8f7860d777b480916eb335f2fb41a07960ea7e1
Harshit Srivastava <72arshit@gmail.com>
2025-07-21 18:15:35 +0530

    user

--- backend/tripmitra/src/main/java/com/acts/tripmitra/dto/UserDto.java
+++ backend/tripmitra/src/main/java/com/acts/tripmitra/dto/UserDto.java
@@ -2,4 +2,6 @@ package com.acts.tripmitra.dto;
 
 public class UserDto {
 
+	
+	
 }
--- backend/tripmitra/src/main/java/com/acts/tripmitra/entity/User.java
+++ backend/tripmitra/src/main/java/com/acts/tripmitra/entity/User.java
@@ -1,5 +1,17 @@
 package com.acts.tripmitra.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
+	
 }
