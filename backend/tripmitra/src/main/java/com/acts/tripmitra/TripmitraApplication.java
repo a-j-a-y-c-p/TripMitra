cdb8cc5213363734cc930ccf20a7e07d92d35f35
Ajay Saini <ajaysaini0505@gmail.com>
2025-07-21 16:24:56 +0530

    added securityConfig and env config

--- backend/tripmitra/src/main/java/com/acts/tripmitra/TripmitraApplication.java
+++ backend/tripmitra/src/main/java/com/acts/tripmitra/TripmitraApplication.java
@@ -3,10 +3,23 @@ package com.acts.tripmitra;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class TripmitraApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure().load();
+
+		System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
+		System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
+		System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		System.setProperty("SPRING_USERNAME", dotenv.get("SPRING_USERNAME"));
+		System.setProperty("SPRING_PASSWORD", dotenv.get("SPRING_PASSWORD"));
+
 		SpringApplication.run(TripmitraApplication.class, args);
 	}
 
