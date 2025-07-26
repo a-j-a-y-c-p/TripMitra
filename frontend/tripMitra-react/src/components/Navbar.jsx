cac3065c127b031f554596517cff63ba159dd4ba
Ajay Saini <ajaysaini0505@gmail.com>
2025-07-26 20:33:13 +0530

    created navbar with bootstrap

--- frontend/tripMitra-react/src/components/Navbar.jsx
+++ frontend/tripMitra-react/src/components/Navbar.jsx
@@ -1,24 +1,36 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-import './Navbar.css'
+
 
 const Navbar = () => {
 
-  var currentLinkClass = "currectNavLink col-auto mx-3 px-2"
-  var linkClass = "NavLink col-auto mx-3 px-2"
-
   return (
-    <nav className='row bg-primary text-success justify-content-between min-h-full'>
-      <div className='col-4 flex-grow-1'>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGvQHbmrL1LUyjF08sKQ5JKeX3EaUtdy7_1g&s" alt="logo" width={30} />
-      </div>
-      <div className='col-auto row mx-2'>
-        <NavLink className={(e)=>{return e.isActive?currentLinkClass:linkClass}} to="/">Home</NavLink>
-        <NavLink className={(e)=>{return e.isActive?currentLinkClass:linkClass}} to="/dashboard">Dashboard</NavLink>
-        <NavLink className={(e)=>{return e.isActive?currentLinkClass:linkClass}} to="/login">Login</NavLink>
-      </div>
-
-    </nav>
+    <nav class="navbar navbar-expand-lg bg-dark border-bottom border-body" data-bs-theme="dark">
+  <div class="container-fluid">
+    <NavLink class="navbar-brand" href="/">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGvQHbmrL1LUyjF08sKQ5JKeX3EaUtdy7_1g&s" alt="logo" width={30} />
+    </NavLink>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-0 ms-lg-auto">
+        <li class="nav-item">
+          <NavLink className={({ isActive }) => isActive ? "nav-link active" : "nav-link"} aria-current="page" to="/">Home</NavLink>
+        </li>
+        <li class="nav-item">
+          <NavLink className={({ isActive }) => isActive ? "nav-link active" : "nav-link"} to="/">Dashboard</NavLink>
+        </li>
+        <li class="nav-item">
+          <NavLink className={({ isActive }) => isActive ? "nav-link active" : "nav-link"} to="/login">Login</NavLink>
+        </li>
+        <li class="nav-item">
+          <NavLink className={({ isActive }) => isActive ? "nav-link active" : "nav-link"} to="/signup">Signup</NavLink>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
   )
 }
 
