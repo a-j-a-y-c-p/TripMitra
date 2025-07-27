3acc4ff469f885adadcefe94af56efe361c1fb56
Ajay Saini <ajaysaini0505@gmail.com>
2025-07-27 22:04:14 +0530

    enabled cors, modified trip get trip APIs, implemented admin dashboard ui

---
+++ backend/tripmitra/src/main/java/com/acts/tripmitra/security/WebConfig.java
@@ -0,0 +1,23 @@
+package com.acts.tripmitra.security;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${frontend.url}")
+    private String frontendUrl;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Allow all endpoints
+//                .allowedOrigins(frontendUrl) // Allow React's origin
+                .allowedOrigins("http://localhost:5173") // Allow React's origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow HTTP methods
+                .allowedHeaders("*") // Allow all headers
+                .allowCredentials(true); // Allow cookies and credentials if needed
+    }
+}
