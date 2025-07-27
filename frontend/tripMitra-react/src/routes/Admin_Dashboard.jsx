fee39697fdd820dba7161ff43f69cdec5ec32340
Ajay Saini <ajaysaini0505@gmail.com>
2025-07-27 18:36:25 +0530

    implemented admin dashboard

--- frontend/tripMitra-react/src/routes/Admin_Dashboard.jsx
+++ frontend/tripMitra-react/src/routes/Admin_Dashboard.jsx
@@ -1,8 +1,51 @@
 import React from 'react'
+import '../css/Admin_Dashboard.css'
 
 const Admin_Dashboard = () => {
   return (
-    <div>Admin_Dashboard</div>
+    <div className="container-fluid">
+      <div className='row m-5 g-4'>
+
+        <div className="col-4">
+          <div className="card custom-card text-bg-danger" >
+            <div className="card-header custom-card-header text-center">
+              Manage Users
+            </div>
+            <div className="card-body custom-card-body">
+              <p className="card-text">Add user, block user, remove user, Manage user permissions </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-4">
+          <div className="card custom-card text-bg-danger" >
+            <div className="card-header custom-card-header text-center">Manage trips</div>
+            <div className="card-body custom-card-body">
+              <p className="card-text">Delete trips</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-4">
+          <div className="card custom-card text-bg-danger" >
+            <div className="card-header custom-card-header text-center">Active trips</div>
+            <div className="card-body custom-card-body">
+              <p className="card-text">See current active trips</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-4">
+          <div className="card custom-card text-bg-danger" >
+            <div className="card-header custom-card-header text-center">Cancelled trips</div>
+            <div className="card-body custom-card-body">
+              <p className="card-text">See cancelled trips</p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
