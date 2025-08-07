e9908d7cb369c92c32935156e59b7bdb78c8b96a
Harshit Srivastava <72arshit@gmail.com>
2025-08-07 19:40:39 +0530

    new api added in backend for changing role of user

--- backend/tripmitra/src/main/java/com/acts/tripmitra/controllers/UserController.java
+++ backend/tripmitra/src/main/java/com/acts/tripmitra/controllers/UserController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -61,6 +62,11 @@ public class UserController {
     @GetMapping("/getAllTrip/{id}")
     public List<Trip> getAllTripsByUserId(@PathVariable Integer id){
     	return userService.getAllTripsByUserId(id);
+    }
+    
+    @PatchMapping("/changeRole/{id}")
+    public String changeRoleOfUser(@PathVariable Integer id) {
+    	return userService.changeRoleOfUser(id);
     }
 
 }
